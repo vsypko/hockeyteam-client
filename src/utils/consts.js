7f3e5b82d4d2061ccc0b1591cb2385be0a19c22c
Volodymyr Sypko <vsypko.dev@gmail.com>
2022-03-23 06:58:35 +0200

    commit 23032022-02

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,4 @@
-// export const API_URL = 'https://vs-hockey-server.herokuapp.com/api'
-export const API_URL = 'http://192.168.1.109:5000/api'
-// export const SOCKET_URL = 'ws://vs-hockey-server.herokuapp.com/echo'
-export const SOCKET_URL='ws://192.168.1.109:5000/echo'
+export const API_URL = 'https://vs-hockey-server.herokuapp.com/api'
+// export const API_URL = 'http://192.168.1.109:5000/api'
+export const SOCKET_URL = 'ws://vs-hockey-server.herokuapp.com/echo'
+// export const SOCKET_URL='ws://192.168.1.109:5000/echo'
